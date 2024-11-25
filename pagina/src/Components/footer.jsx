adffa9760110468707d2d3664f4b3130c52c6a66
Simon <as.escudero@alumno.etec.um.edu.ar>
2024-11-25 11:06:52 -0300

    forms

--- pagina/src/Components/footer.jsx
+++ pagina/src/Components/footer.jsx
@@ -1,13 +1,94 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export const Footer = () => {
+  const [rating, setRating] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [email, setEmail] = useState('');
+  const [content, setContent] = useState(''); 
+  const dialogRef = useRef(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const respuesta = await axios.get("/app/users/ratings");
+        console.log(respuesta);
+        setRating(respuesta.data);
+      } catch (error) {
+        console.log("Error");
+      }
+    };
+    fetchData();
+  }, []);
+
+  const calificar = async () => {
+    const rating = parseInt(prompt('Califica nuestra página del 1 al 5:'));
+    if (rating >= 1 && rating <= 5) {
+      try {
+        const respuesta = await axios.post("/app/users/calificar", JSON.stringify({ rating: rating }));
+        console.log(respuesta);
+        alert(respuesta);
+      } catch (error) {
+        console.log("Error");
+        alert(error);
+      }
+    } else {
+      alert("Calificación inváida. Por favor ingrese un número del 1 al 5.");
+    }
+  };
+
+  const openDialog = () => {
+    dialogRef.current.showModal();
+    setIsOpen(true);
+  };
+
+  const closeDialog = () => {
+    dialogRef.current.close();
+    setIsOpen(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/app/users/soporte", { email, content });
+      alert(response.data);
+    } catch (error) {
+      alert("No se pudo enviar el mensaje");
+    }
+    closeDialog();
+  };
+
   return (
     <footer>
       <div className="footer-content">
         <div className="footer-contact">
           <h2>Contacto</h2>
           <p>Si tienes alguna pregunta sobre las razas de perros, no dudes en contactarnos.</p>
-          <a href="#" className="btn-contact">Contáctanos</a>
+          <button onClick={openDialog} className="btn-contact">Contáctanos</button>
+          <dialog ref={dialogRef}>
+            <form>
+                <h2>Contáctanos</h2>
+                <label for="supportEmail">Email:</label>
+                <input
+                  type="email"
+                  id="supportEmail"
+                  name="email"
+                  value={email}
+                  onChange={() => setEmail(e.target.value)}
+                  required/>
+                <br/>
+                <label for="content">Contenido:</label>
+                <input
+                  type="textarea"
+                  id="content"
+                  name="content"
+                  value={content}
+                  onChange={() => setContent(e.target.value)}
+                  required/>
+                <br/>
+                <button className="btn-contact" type="submit" onClick={handleSubmit}>Enviar</button>
+                <button className="btn-contact" type="button" onClick={closeDialog}>Cancelar</button>
+            </form>
+          </dialog>
         </div>
         <div className="footer-social">
           <h2>Síguenos en las redes sociales</h2>
@@ -15,6 +96,7 @@ export const Footer = () => {
             <li><a href="https://www.facebook.com/tuempresa" target="_blank" rel="noopener noreferrer">Facebook</a></li>
             <li><a href="https://www.twitter.com/tuempresa" target="_blank" rel="noopener noreferrer">Twitter</a></li>
             <li><a href="https://www.instagram.com/tuempresa" target="_blank" rel="noopener noreferrer">Instagram</a></li>
+            <li>{rating}⭐ <span><button className='btn-contact' onClick={calificar}>Calificanos</button></span></li>
           </ul>
         </div>
       </div>
